1c96499fbfd256c15b28cd14d72208d496af172f
Eduardo Avelar <eavelardev@gmail.com>
2015-10-03 12:15:22 -0500

    Update i3wm.sh

--- i3wm.sh
+++ i3wm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install --assume-yes sudo xorg i3 terminator git nodm zsh build-essential module-assistant
+apt-get install --assume-yes sudo xorg i3 terminator git curl nodm zsh build-essential module-assistant
 adduser user sudo
 sed -i s/=false/=true/g /etc/default/nodm
 sed -i s/=root/=user/g /etc/default/nodm
