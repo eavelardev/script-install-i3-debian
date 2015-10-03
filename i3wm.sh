055c570550a3cdb84c1243ddedebe58a319b860d
Eduardo Avelar <eavelardev@gmail.com>
2015-10-03 11:48:31 -0500

    Update i3wm.sh

--- i3wm.sh
+++ i3wm.sh
@@ -3,7 +3,6 @@ apt-get install --assume-yes sudo xorg i3 terminator git nodm zsh build-essentia
 adduser user sudo
 sed -i s/=false/=true/g /etc/default/nodm
 sed -i s/=root/=user/g /etc/default/nodm
-m-a prepare
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 exit
 reboot
