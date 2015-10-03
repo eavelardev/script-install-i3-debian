4f676ff12a069643d88a01cbfedc27dd69339f85
Eduardo Avelar <eavelardev@gmail.com>
2015-10-03 10:35:31 -0500

    Update i3wm.sh

--- i3wm.sh
+++ i3wm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get install sudo --assume-yes xorg i3 terminator git nodm zsh build-essential module-assistant
 adduser user sudo
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sed -i s/=false/=true/g /etc/default/nodm
 sed -i s/=root/=user/g /etc/default/nodm
 m-a prepare
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 reboot
