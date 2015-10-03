42d56ab2b08aeaf78514ba738658a96ab7a095b5
Eduardo Avelar <eavelardev@gmail.com>
2015-10-03 11:47:04 -0500

    Update i3wmtest.sh

--- i3wmtest.sh
+++ i3wmtest.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-apt-get install --assume-yes git zsh
+apt-get install --assume-yes sudo xorg i3 terminator git nodm zsh build-essential module-assistant
+adduser user sudo
+sed -i s/=false/=true/g /etc/default/nodm
+sed -i s/=root/=user/g /etc/default/nodm
+m-a prepare --assume-yes
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+exit
 reboot
